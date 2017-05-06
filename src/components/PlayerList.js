935de74cdc1e36d1258a4025d10832f6bb14eba9
jacques mouette <j.mouette@gmail.com>
2017-05-06 20:07:31 -0300

    altera quantidade de colunas do grid de acordo com width da janela

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,18 +2,43 @@ import React from 'react';
 import { PlayerCard } from './common';
 
 const containerStyle = {
-  margin: '10px',
-  padding: '10px',
-  backgroundColor: 'rgb(200,200,200)',
-  height: '50%',
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '10px',
-  gridAutoRows: 'minmax(100px, auto)'
+  base: {
+    margin: '10px',
+    padding: '10px',
+    backgroundColor: 'rgb(200,200,200)',
+    height: '50%',
+    display: 'grid',
+    gridGap: '10px',
+    gridAutoRows: 'minmax(100px, auto)',
+  },
+  fourCol: {
+    gridTemplateColumns: 'repeat(4, 1fr)',
+  },
+  threeCol: {
+    gridTemplateColumns: 'repeat(3, 1fr)',
+  },
+  twoCol: {
+    gridTemplateColumns: 'repeat(2, 1fr)',
+  },
+  oneCol: {
+    gridTemplateColumns: 'repeat(1, 1fr)',
+  }
 };
 
-const PlayerList = ({ playerList = [] }) => (
-  <div style={containerStyle}>
+const getCols = (width) => {
+  if (width < 560) {
+    return {...containerStyle.base, ...containerStyle.oneCol};
+  } else if (width < 840) {
+    return {...containerStyle.base, ...containerStyle.twoCol};
+  } else if (width < 1224) {
+    return {...containerStyle.base, ...containerStyle.threeCol};    
+  } else {
+    return {...containerStyle.base, ...containerStyle.fourCol};    
+  }
+}
+
+const PlayerList = ({ playerList = [], width }) => (
+  <div style={getCols(width)}>
     {playerList.map((player) => (
       <PlayerCard
         key={player.id}
