fb739f86949057e46d95844a7f86610d0e067dea
jacques mouette <j.mouette@gmail.com>
2017-05-06 20:24:34 -0300

    styling PlayerCard; Finaliza Parte 2

--- src/components/common/PlayerCard.js
+++ src/components/common/PlayerCard.js
@@ -1,13 +1,41 @@
 import React from 'react';
 
-const cardStyle = {
-  backgroundColor: '#828282',
-  minWidth: '250px'
+const styles = {
+  cardStyle: {
+    backgroundColor: '#828282',
+    minWidth: '250px',
+    padding: '5px'
+  },
+  avatarStyle: {
+    float: 'left',
+    marginRight: '25px'
+  },
+  nickStyle: {
+    fontSize: '20px',
+    fontWeight: 'bold'
+  },
+  buttonStyle: {
+    float: 'right'
+  }
 };
 
 const PlayerCard = ({ player }) => (
-  <div style={cardStyle}>
-    <p>{player.username}</p>
+  <div style={styles.cardStyle}>
+    <button 
+      style={styles.buttonStyle}
+      onClick={() => alert(`${player.username} entrou no lobby!`)}
+    >
+      Entrar no Lobby
+    </button>
+    <img src={player.avatar_medium} alt='avatar' style={styles.avatarStyle}/>
+    <p style={styles.nickStyle}>{player.username}</p>
+    {
+      player.is_available 
+      ? 
+      (<p>Possui time</p>) 
+      :
+      (<p>Não possui time</p>)
+    }
   </div>
 );
 
